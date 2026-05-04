b9af7eee6063c7d99bd3557416e540bbeb5a122b
Aninymouse <aninymouse3@gmail.com>
2026-05-04 03:18:38 -0400

    M CRABO ABILITY

--- src/data/pokemon/species_info/gen_7_families.h
+++ src/data/pokemon/species_info/gen_7_families.h
@@ -1493,7 +1493,7 @@ const struct SpeciesInfo gSpeciesInfoGen7[] =
         .friendship = STANDARD_FRIENDSHIP,
         .growthRate = GROWTH_MEDIUM_FAST,
         .eggGroups = MON_EGG_GROUPS(EGG_GROUP_WATER_3),
-        .abilities = { ABILITY_HYPER_CUTTER, ABILITY_IRON_FIST, ABILITY_ANGER_POINT },
+        .abilities = { ABILITY_IRON_FIST, ABILITY_IRON_FIST, ABILITY_IRON_FIST },
         .bodyColor = BODY_COLOR_WHITE,
         .speciesName = _("Crabominable"),
     #if P_MODIFIED_MEGA_CRIES
